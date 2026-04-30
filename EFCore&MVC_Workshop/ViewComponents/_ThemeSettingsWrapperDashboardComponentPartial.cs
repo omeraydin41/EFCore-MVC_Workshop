f3d1073bfdb5a6fa4ee92788b47bdae54351f6ce
omeraydin41 <omerenver1317@gmail.com>
2026-05-01 01:10:59 +0300

    ENG : To facilitate the management of the design code in Indext.cshtml using the Partial View approach, the Theme, Left Menu, Card Menu, and Right Menu sections have been separated into classes and individual views.
    TUR : Indext.cshtml içindeki tasarım kodlarının Partial View mantığı ile kolay yönetilmesi adına Tema - Sol Menü - Kart Menü - Sağ Menü kısımları classlara ve ayrı ayrı viewlere ayrıldı.

---
+++ EFCore&MVC_Workshop/ViewComponents/_ThemeSettingsWrapperDashboardComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore_MVC_Workshop.ViewComponents
+{
+    public class _ThemeSettingsWrapperDashboardComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
