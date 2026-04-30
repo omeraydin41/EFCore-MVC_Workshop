038b56da1574f8d2cb3c7c2374a83fa1f3da4c86
omeraydin41 <omerenver1317@gmail.com>
2026-04-30 23:42:54 +0300

    ENG : To make it easier to manage long blocks of code, the View Component logic was refactored from the Index.cshtml page by moving the header and navpar properties into separate classes.
    TUR : Uzun kod bloklarını kolay yönetebilmek için View Komponent mantığını Indext.cshtml sayfasından header ve navpar propertylerini ayrı classlara yerleştirme işlemi yapıldı.

---
+++ EFCore&MVC_Workshop/ViewComponents/_NavbarDashboardComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore_MVC_Workshop.ViewComponents
+{
+    public class _NavbarDashboardComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
