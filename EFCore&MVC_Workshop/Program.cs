52e1e6d1b0e998bfff947847c65384ac3f39618f
omeraydin41 <omerenver1317@gmail.com>
2026-05-03 13:40:05 +0300

    ENG : At this stage, I used the COUNT and AVERAGE methods from the EF methods and successfully transferred them to the UI. I used ViewBag and integrated it into the cshtml code. I learned about lambda expressions.
    TUR : Bu aşamada EF methodlarından COUNT VE AVERAGE methodlarının kullandım ve başarıyla UI kısmına aktardım. ViewBag kullanıldı ve cshtml koduna entegre ettim.
    Lamda mantığı öğrendim.

--- EFCore&MVC_Workshop/Program.cs
+++ EFCore&MVC_Workshop/Program.cs
@@ -1,6 +1,9 @@
+using EFCore_MVC_Workshop.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<StoreContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
--- EFCore&MVC_Workshop/ViewComponents/_CardStatisticDashboardComponentPartial.cs
+++ EFCore&MVC_Workshop/ViewComponents/_CardStatisticDashboardComponentPartial.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFCore_MVC_Workshop.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EFCore_MVC_Workshop.ViewComponents
 {
     public class _CardStatisticDashboardComponentPartial :ViewComponent
     {
+        private readonly StoreContext _context;//DI ya uygun olarak Bağlantı addresını tutan StoreContext classından nesne aldık db ile bağlantı için.
+
+        public _CardStatisticDashboardComponentPartial(StoreContext storeContext)// CTRL . ile constructor alındı 
+        {
+            this._context = storeContext;
+        }
+
         public IViewComponentResult Invoke()
         {
-          return View();
+            //COUNT 
+            ViewBag.totalCustomerCount  = _context.Customers.Count(); //Customer tablosu toplam satır sayısı 
+            ViewBag.totalCategoryCount  =_context.Categories.Count(); //Categories tablosu satır sayısını verir
+            ViewBag.totalProductCount   =_context.Products.Count();   //Product tablosu toplam satır sayısı 
+            //ViewBag : Backend tarfından UI tarafına veri taşınmasını sağlar . veri tekıl değil liste halındede olabılır.
+            //count : tablodaki  veri sayısını getiren EF methodudur
+
+            //AVG : sütün ortalaması veriri
+            ViewBag.avgCustomerBalance = _context.Customers.Average(x => x.Balance).ToString("N2") + " TL";
+            //Customers tablosu Balance(bakiye) alnının(sütun) ortalamsını verir ve , den sonra son ıkı basamağı sadece verir.
+            //x=>x : LAMBDA anonim fonksion tanımlama .fonksiyon tanımlama ona isim verip çağırmak yerine Lambda ise bu süreci kısaltır.
+
+            return View();
         }
     }
 }
