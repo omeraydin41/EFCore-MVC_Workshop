77056adf003800a9e274e416948b1304c1f04084
omeraydin41 <omerenver1317@gmail.com>
2026-05-07 17:09:50 +0300

    ENG : The Add, Remove, Find, and Update methods were used for the Category table and successfully implemented in the UI. For the Product table, a controller, methods, and views were added.
    TUR : Category tablosu için Add - remove -  find - update methodları kullanıldı  ve UI ye başarılı şekilde aktarıldı  .Product Tablosu için Controller ,methodlar ve viewler eklendi.

--- EFCore&MVC_Workshop/Controllers/CategoryController.cs
+++ EFCore&MVC_Workshop/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using EFCore_MVC_Workshop.Context;
+using EFCore_MVC_Workshop.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EFCore_MVC_Workshop.Controllers
@@ -14,8 +15,53 @@ namespace EFCore_MVC_Workshop.Controllers
 
         public IActionResult CategoryList()
         {
-            var values= _context.Categories.ToList();//ToList() ile cATEGORİES tablsundaki verileri liste halınde aldık 
+            var values = _context.Categories.ToList();//ToList() ile cATEGORİES tablsundaki verileri liste halınde aldık 
             return View(values);
+        }
+
+        //yenı kategori ekleme işlemi
+        [HttpGet]// get isteği ile çalışacak bir aksiyon metodu olduğunu belirtir // sayfa ilk açıldığında boş bir formun gelmesini sağlar
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)//CreateCategory adı Categoryist ana viewinde buton linki olarak verilmişti
+        {
+            category.CategoryStatus = false;
+            _context.Categories.Add(category);//veri tabanına ekleme işlemi
+            _context.SaveChanges();//değişiklikleri kaydetme işlemi
+            return RedirectToAction("CategoryList");//ekleme işlemi tamamlandıktan sonra kategori listesine yönlendirme işlemi
+        }
+
+        //silme işlemi attribute yerine link alır 
+        //silme işlemi ID ye göre yapılır
+
+        //ID BULUNMALI - 
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);//veri tabanında id ye göre arama işlemi
+            _context.Categories.Remove(value);//veri tabanından silme işlemi
+            _context.SaveChanges();//değişiklikleri kaydetme işlemi
+            return RedirectToAction("CategoryList");//silme işlemi tamamlandıktan sonra kategori listesine yönlendirme işlemi
+        }
+
+
+
+        [HttpGet]//guncelleme işlemi ıs değeri uzerinden yapılır
+        public IActionResult UpdateCategory(int id)
+        {
+            //guncellenecek olan veri sayfaya taşınır
+            var value = _context.Categories.Find(id);//veri tabanında id ye göre arama işlemi
+            return View(value);//guncellenecek olan veri sayfaya taşınır
+        }
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);//guncelleme işlemi
+            _context.SaveChanges();//değişiklikleri kaydetme işlemi
+            return RedirectToAction("CategoryList");//guncelleme işlemi tamamlandıktan sonra kategori listesine yönlendirme işlemi
         }
     }
 }
---
+++ EFCore&MVC_Workshop/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using EFCore_MVC_Workshop.Context;
+using EFCore_MVC_Workshop.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore_MVC_Workshop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public ProductController(StoreContext context)//DI İÇİN 
+        {
+            _context = context;
+        }
+
+        public IActionResult ProductList()
+        {
+            var values=_context.Products.ToList();//product tablosunu liste olarak aldık.
+            return View(values);
+        }
+
+
+        #region 
+        //ÜRÜN EKLEME 
+        [HttpGet]//sayfayı getirecek method 
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]//değişikliği yapacak olan method 
+        public IActionResult CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("ProductList");//ProductList methodunun viewine (bu controllerdeki ana sayfamız)
+        }
+        #endregion
+
+        //silme işlemi : id üzerinden yapılır 
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = _context.Products.Find(id);
+            _context.Products.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+
+
+        #region
+        //ÜRÜN GÜNCELLEME
+        [HttpGet] //sayfayı getirecek olan method
+        public IActionResult UpdateProduct(int id)
+        {
+            var value= _context.Products.Find(id);//güncellenecek olan ürünü id üzerinden bulduk
+            return View(value);//güncellenecek olan ürünü sayfaya taşıdık
+        }
+        [HttpPost]//değişikliği yapacak olan method
+        public IActionResult UpdateProduct(Product product)
+        {
+            _context.Products.Update(product);//güncelleme işlemi
+            _context.SaveChanges();
+            return RedirectToAction("ProductList");//güncelleme işlemi tamamlandıktan sonra ürün listesine yönlendirme işlemi
+        }
+
+
+        #endregion
+    }
+}
